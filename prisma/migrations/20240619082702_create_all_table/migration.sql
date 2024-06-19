2e9476b818ad40b984bbfd655c775d903ef4398b
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-06-19 18:01:23 +0700

    fix-all

---
+++ prisma/migrations/20240619082702_create_all_table/migration.sql
@@ -0,0 +1,282 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(5) NOT NULL DEFAULT 'user',
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(12) NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(191) NULL,
+    `avatar` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phone_key`(`phone`),
+    UNIQUE INDEX `User_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Address` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `province` VARCHAR(50) NOT NULL,
+    `district` VARCHAR(50) NOT NULL,
+    `sub_district` VARCHAR(50) NOT NULL,
+    `post_code` VARCHAR(5) NOT NULL,
+    `village` VARCHAR(50) NOT NULL,
+    `rt` INTEGER NOT NULL,
+    `rw` INTEGER NOT NULL,
+    `detail_address` VARCHAR(100) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Address_user_id_key`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subscription` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `is_pay` BOOLEAN NOT NULL DEFAULT true,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_detail` (
+    `id` VARCHAR(191) NOT NULL,
+    `subscription_id` VARCHAR(191) NOT NULL,
+    `num_of_people` INTEGER NOT NULL,
+    `meals_per_week` INTEGER NOT NULL,
+    `total_serving` INTEGER NOT NULL,
+    `box_price` INTEGER NOT NULL,
+    `price_per_serving` INTEGER NOT NULL,
+    `shipping_price` INTEGER NOT NULL,
+    `total_price` INTEGER NOT NULL,
+    `start_date` DATETIME(3) NULL,
+    `end_date` DATETIME(3) NULL,
+    `pay_at` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `subscription_detail_subscription_id_key`(`subscription_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_preferences` (
+    `subscription_detail_id` VARCHAR(191) NOT NULL,
+    `preferences_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`subscription_detail_id`, `preferences_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_delivery` (
+    `id` VARCHAR(191) NOT NULL,
+    `subscription_detail_id` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `status` VARCHAR(20) NOT NULL DEFAULT 'packed',
+    `total_recipe` INTEGER NOT NULL,
+    `accepted_at` DATETIME(3) NULL,
+    `send_at` DATETIME(3) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_delivery_recipe` (
+    `subscription_delivery_id` VARCHAR(191) NOT NULL,
+    `recipe_id` VARCHAR(191) NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`subscription_delivery_id`, `recipe_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_plan` (
+    `id` INTEGER NOT NULL,
+    `price_per_serving` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscription_plan_preferences` (
+    `subscription_plan_id` INTEGER NOT NULL,
+    `preferences_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`subscription_plan_id`, `preferences_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `available_food` (
+    `id` VARCHAR(191) NOT NULL,
+    `start_date` DATETIME(3) NOT NULL,
+    `end_date` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `available_food_start_date_key`(`start_date`),
+    UNIQUE INDEX `available_food_end_date_key`(`end_date`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `available_food_recipe` (
+    `available_food_id` VARCHAR(191) NOT NULL,
+    `recipe_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`available_food_id`, `recipe_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Recipe` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `photo` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Recipe_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `recipe_allergy` (
+    `recipe_id` VARCHAR(191) NOT NULL,
+    `allergy_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`recipe_id`, `allergy_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `recipe_material` (
+    `recipe_id` VARCHAR(191) NOT NULL,
+    `material_id` VARCHAR(191) NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `unit` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`recipe_id`, `material_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `recipe_preferences` (
+    `recipe_id` VARCHAR(191) NOT NULL,
+    `preferences_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`recipe_id`, `preferences_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Instruction` (
+    `recipe_id` VARCHAR(191) NOT NULL,
+    `step` INTEGER NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`recipe_id`, `step`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Allergy` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Allergy_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Material` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `photo` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Material_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Preferences` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `photo` VARCHAR(100) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Preferences_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Address` ADD CONSTRAINT `Address_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Subscription` ADD CONSTRAINT `Subscription_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_detail` ADD CONSTRAINT `subscription_detail_subscription_id_fkey` FOREIGN KEY (`subscription_id`) REFERENCES `Subscription`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_preferences` ADD CONSTRAINT `subscription_preferences_subscription_detail_id_fkey` FOREIGN KEY (`subscription_detail_id`) REFERENCES `subscription_detail`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_preferences` ADD CONSTRAINT `subscription_preferences_preferences_id_fkey` FOREIGN KEY (`preferences_id`) REFERENCES `Preferences`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_delivery` ADD CONSTRAINT `subscription_delivery_subscription_detail_id_fkey` FOREIGN KEY (`subscription_detail_id`) REFERENCES `subscription_detail`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_delivery_recipe` ADD CONSTRAINT `subscription_delivery_recipe_subscription_delivery_id_fkey` FOREIGN KEY (`subscription_delivery_id`) REFERENCES `subscription_delivery`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_delivery_recipe` ADD CONSTRAINT `subscription_delivery_recipe_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_plan_preferences` ADD CONSTRAINT `subscription_plan_preferences_subscription_plan_id_fkey` FOREIGN KEY (`subscription_plan_id`) REFERENCES `subscription_plan`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subscription_plan_preferences` ADD CONSTRAINT `subscription_plan_preferences_preferences_id_fkey` FOREIGN KEY (`preferences_id`) REFERENCES `Preferences`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `available_food_recipe` ADD CONSTRAINT `available_food_recipe_available_food_id_fkey` FOREIGN KEY (`available_food_id`) REFERENCES `available_food`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `available_food_recipe` ADD CONSTRAINT `available_food_recipe_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_allergy` ADD CONSTRAINT `recipe_allergy_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_allergy` ADD CONSTRAINT `recipe_allergy_allergy_id_fkey` FOREIGN KEY (`allergy_id`) REFERENCES `Allergy`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_material` ADD CONSTRAINT `recipe_material_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_material` ADD CONSTRAINT `recipe_material_material_id_fkey` FOREIGN KEY (`material_id`) REFERENCES `Material`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_preferences` ADD CONSTRAINT `recipe_preferences_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `recipe_preferences` ADD CONSTRAINT `recipe_preferences_preferences_id_fkey` FOREIGN KEY (`preferences_id`) REFERENCES `Preferences`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Instruction` ADD CONSTRAINT `Instruction_recipe_id_fkey` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
