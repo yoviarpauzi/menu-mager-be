f4127124684641310c3c3e2b74bc840a6e57c757
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-06-19 16:05:47 +0700

    fix

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import "src/application/web.js"
+import app from "./src/application/web.js";
+import logger from "./src/application/logger.js";
+
+const port = 9000;
+
+app.listen(port, () => {
+  logger.info(`Server running with port ${port}`);
+});
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import app from "./application/web.js";
-import logger from "./application/logger.js";
-
-const port = 9000;
-
-app.listen(port, () => {
-  logger.info(`Server running with port ${port}`);
-});
